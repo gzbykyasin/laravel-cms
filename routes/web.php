4d1f3dc931dd12949ee88f75c572388e219a4352
Yasin <gzbyk.yasinn@gmail.com>
2019-03-07 00:16:03 +0300

    view controller complate

--- routes/web.php
+++ routes/web.php
@@ -20,21 +20,6 @@ Route::get('/clear',function(){
 });
 Auth::routes();
 
-Route::namespace('Web')->group(function (){
-    Route::get('/', 'HomePageController@index')->name('home');
-    Route::get('/kurumsal.html','StandartPagesController@about')->name('about');
-    Route::get('/iletisim.html','StandartPagesController@contact')->name('contact');
-    Route::post('/sendForm','StandartPagesController@sendForm')->name('sendForm');
-    Route::get('/hizmetler','ServicesController@services')->name('services');
-    Route::get('/hizmetler/{slug}.html','ServicesController@service_detail')->name('service_detail');
-    Route::get('/projeler.html','ProjectsController@index')->name('projects');
-    Route::get('/video-galeri.html','ServicesController@video_gallery')->name('video_gallery');
-    Route::get('/blog','BlogController@index')->name('blog');
-    Route::get('/blog/{slug}.html','BlogController@blog_detail')->name('blog_detail');
-    Route::get('/kategori','CategoryController@index')->name('category');
-    Route::get('/kategori/{slug}.html','CategoryController@category_detail')->name('category_detail');
-});
-
 Route::namespace('Admin')->prefix('admin')->group(function (){
 
     Route::get('/','DashboardController@index');
